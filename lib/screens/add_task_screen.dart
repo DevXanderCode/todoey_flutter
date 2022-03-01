a6592bf4612e7b72b8c9b817809aed9965099833
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-03-01 19:13:48 +0100

    started created flat button equivalent

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -13,6 +13,34 @@ class AddTaskScreen extends StatelessWidget {
             topRight: Radius.circular(20.0),
           ),
         ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 16.0),
+          child: Column(
+            children: [
+              Text(
+                'Add Task',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.lightBlueAccent,
+                    fontWeight: FontWeight.w500),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      minimumSize: Size(double.infinity, 56.0),
+                      backgroundColor: Colors.lightBlueAccent),
+                  onPressed: null,
+                  child: Text('Add'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
