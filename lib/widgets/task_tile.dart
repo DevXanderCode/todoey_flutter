c54540a8621bd446f835a205423339d6438e23c2
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-03-02 03:31:20 +0100

    worked on changing the is checked state when check box is tapped

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void onChange(bool value) => setState(() {
+        isChecked = value;
+      });
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('My First Task'),
-      trailing: TaskCheckbox(),
+      title: Text(
+        'My First Task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+        isChecked,
+        onChange,
+      ),
     );
   }
 }
 
-class TaskCheckbox extends StatefulWidget {
-  @override
-  State<TaskCheckbox> createState() => _TaskCheckboxState();
-}
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox(this.checkBoxState, this.onChange);
+  final bool checkBoxState;
+  final Function onChange;
 
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Checkbox(
-      value: isChecked,
+      value: checkBoxState,
       activeColor: Colors.lightBlueAccent,
       onChanged: (bool? value) {
-        setState(() {
-          isChecked = value!;
-        });
+        onChange(value);
+        // setState(() {
+        //   isChecked = value!;
+        // });
       },
     );
   }
